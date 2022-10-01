687c82d8560c94ec78d5a19554f99f6416d1fae0
cuichenxinh <cuichenxi@newhuotech.com>
2022-10-01 18:13:50 +0800

    新火文案升级

--- deploy.sh
+++ deploy.sh
@@ -228,7 +228,7 @@ commit+push() {
   if [ $GH_TOKEN ]; then
     # deploy by Travis CI
     # add github token
-    repo="https://"$GH_TOKEN"@github.com/newhuo-tech/trade-docs.git"
+    repo="https://"$GH_TOKEN"@github.com/newhuo-tech/docs.git"
     git remote add origin-pages $repo
     git push --quiet origin-pages $deploy_branch
   else
